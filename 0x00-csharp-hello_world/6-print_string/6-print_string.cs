7a648c2177c61adcc629d604584ea4ce1b60f56f
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-12 21:03:33 +0000

    Print strings and substring

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+        string nine = str.Substring(0, 9);
+		Console.WriteLine($"{str}{str}{str}\n{nine}");
+        }
+}
