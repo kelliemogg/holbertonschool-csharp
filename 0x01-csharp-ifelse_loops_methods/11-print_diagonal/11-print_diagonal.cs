941f488ec0c70297fdff233a0d64280f6798bcd7
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-13 21:45:25 +0000

    nested for loops

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        int x, y;
+        if (length <= 0)
+            Console.WriteLine("\n");
+        for (x = 0; x < length; x++)
+        {
+            for (y = 0; y < x + 1; y++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine("\\");
+        }
+    }
+}
