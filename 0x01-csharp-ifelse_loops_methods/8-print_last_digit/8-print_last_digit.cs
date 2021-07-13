6fe7b857423bae3b77d4229e21f978ebeda18d35
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-13 20:53:28 +0000

    last digit returns

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int lastdigit = number % 10;
+        if (lastdigit < 0)
+            lastdigit = lastdigit * -1;
+        Console.Write(lastdigit);
+        return (lastdigit);
+    }
+}
