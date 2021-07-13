d30099cbfb1b54e9171b06e662e555464ab2d29e
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-13 20:11:01 +0000

    checks if something is lowercase using a bool

---
+++ 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Character
+{
+    public static bool IsLower(char c)
+    {
+        bool result = true;
+        bool result2 = false;
+        if (c >= 'a' && c <= 'z')
+            return (result);
+        else
+            return (result2);
+    }
+}
