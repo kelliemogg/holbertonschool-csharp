8e6ec91283bc3569e5d2eaec2b1dad1e9775db3b
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-13 17:15:37 +0000

    print dec and hexidecimal

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int c = 0; c <= 98; c++)
+                Console.WriteLine("0x{0:X}", c);
+        }
+    }
+}
