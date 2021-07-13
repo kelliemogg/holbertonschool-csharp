f2d328441961971a36c124405695f2039721bc68
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-13 19:49:17 +0000

    if else to print diff combinations of two digits

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int f, s;
+            for (f = 0; f < 9; f++)
+            {
+                for (s = f + 1; s <= 9; s++)
+                {
+                    if (f == s)
+                        continue;
+                    else if (f == 8 && s == 9)
+                        Console.WriteLine("{0}{1}", f, s);
+                    else
+                        Console.Write("{0}{1}, ", f, s);
+                }
+            }
+        }
+    }
+}
