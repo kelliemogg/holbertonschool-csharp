25d3cffdcc90015039921e3b36b1730d23fb80e6
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-13 21:21:14 +0000

    if else to draw some lines

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        int a;
+        if (length <= 0)
+            Console.WriteLine("\n");
+        for (a = 0; a < length; a++)
+        {
+            if (a == length)
+                Console.WriteLine("\n");
+            else
+                Console.Write("_");
+        }
+    }
+}
