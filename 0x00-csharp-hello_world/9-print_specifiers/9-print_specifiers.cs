772b9d66323cde8eb174de18e223d41d8eee41a4
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-13 04:07:19 +0000

    WriteLine using global var

---
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+		Console.WriteLine($"Percent: {percent,1:P}\nCurrency: {currency,0:C}");
+	}
+}
