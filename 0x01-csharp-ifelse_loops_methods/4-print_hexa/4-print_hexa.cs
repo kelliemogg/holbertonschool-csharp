cf89dd80286ce7d91f3a0919e532728765243197
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-13 17:17:14 +0000

    fixed formating

--- 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -7,7 +7,7 @@ namespace _4_print_hexa
         static void Main(string[] args)
         {
             for (int c = 0; c <= 98; c++)
-                Console.WriteLine("0x{0:X}", c);
+                Console.WriteLine("{0} = 0x{0:X}", c);
         }
     }
 }
