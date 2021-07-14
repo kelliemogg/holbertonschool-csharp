ae5eb2d726195a7731de9824a33777b68e506cf6
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-14 21:59:13 +0000

    task 15

---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+        if (myList.Count == 0)
+        {
+            Console.WriteLine("List is empty");
+            return -1;
+        }
+        int max_num = myList[0];
+        foreach (int i in myList)
+        {
+            if (max_num < i)
+                max_num = i;
+        }
+        return max_num;
+    }
+}
+
