c6a8b185e7fb45c7ab26496b2ac3b8644f45b9a7
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-14 19:08:20 +0000

    Returns a value at an index

---
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Array
+{
+    public static int elementAt(int[] array, int index)
+    {
+        if (index > array.Length + 1 || index < 0)
+        {
+            Console.WriteLine("Index out of range");
+            return -1;
+        }
+        else
+        {
+            for (int i = 0; i <= array.Length; i++)
+            {
+                if (i == index)
+                    return array[i];
+            }
+            return -1;
+        }
+    }
+}
