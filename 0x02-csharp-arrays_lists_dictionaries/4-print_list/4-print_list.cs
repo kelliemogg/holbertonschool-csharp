1764422392134ace4d9413790584b170f82c91f0
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-14 20:44:02 +0000

    Adding to a list and printing

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        List<int> lists = new List();
+        for (int i = 0; i < size; i++)
+        {
+            lists.Add(i);
+            Console.Write(i);
+            if (i < size)
+                Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+}
