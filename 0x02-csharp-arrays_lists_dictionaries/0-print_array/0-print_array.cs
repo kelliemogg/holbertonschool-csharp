eb675b489b44e864f00e1a647b6fc3e8da10ed1c
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-14 18:39:47 +0000

    initialize array first

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        else
+        {
+            int[] array = new int[size];
+            if (size == 0)
+            {
+                Console.WriteLine(""); 
+                return array;
+            }
+            else
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = i;
+                    Console.Write(i);
+                    if (i < array.Length - 1)
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine("");
+                return array;
+            }
+        }
+    }
+}
