fdc0540ae31590e14f685cc5f6658d3e18bbde1b
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-15 22:56:23 +0000

    Node is its own data type

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class LList
+{
+    public static LinkedListNode<int> Add(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> nood = new LinkedListNode<int>(n);
+        myLList.AddFirst(nood);
+        return nood;
+    }
+}
