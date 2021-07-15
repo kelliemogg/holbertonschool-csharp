8edb0044fe8ecf4fc455afdd7716fdf1628a2103
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-15 22:44:32 +0000

    linkedlist

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        int i = 0;
+        for (int itr = 0; itr < myLList.Count; itr++)
+        {
+            i += 1;
+        }
+        return i;
+    }
+}
