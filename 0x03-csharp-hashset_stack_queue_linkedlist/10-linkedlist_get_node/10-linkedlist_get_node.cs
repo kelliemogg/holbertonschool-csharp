efebb2e794d7f285e7e57b83c0a374e653db88ee
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-15 23:57:58 +0000

    return value at an index

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        var node = 0;
+        if (myLList.Count == 0)
+            return 0;
+        foreach (int val in myLList)
+        {
+            if (node == n)
+                return val;
+            node ++;
+        }
+        return 0;
+    }
+}
+
