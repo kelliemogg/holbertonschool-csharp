fb67ee26b07572b464183d472f91783bb775de97
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-15 23:38:54 +0000

    null check

--- 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -7,7 +7,7 @@ class LList
     public static int Pop(LinkedList<int> myLList)
     {
         int val = 0;
-        if (myLList == null)
+        if (myLList.Count == 0)
             return 0;
         var node = myLList.First;
         val = node.Value;
