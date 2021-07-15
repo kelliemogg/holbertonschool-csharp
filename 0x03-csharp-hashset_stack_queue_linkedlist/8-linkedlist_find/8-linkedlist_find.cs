3ee850164c5afea84c1fb5b163b8d75c49956d7d
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-15 23:12:21 +0000

    Searching Linked Lists in C#

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int count = 0;
+        for (var node = myLList.First; node != null; node = node.Next, count++)
+        {
+            if (value.Equals(node.Value))
+                return count;
+        }
+        return -1;
+    }
+}
