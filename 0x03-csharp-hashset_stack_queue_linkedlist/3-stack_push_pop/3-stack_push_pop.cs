1fd437b7e98a1afd1ad56fc82af21455c950bff7
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-15 18:50:31 +0000

    manipulating the stack

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Boolean condT = true;
+        int count = 0;
+        if (aStack == null)
+            Console.WriteLine("Stack is empty");
+        count = aStack.Count;
+        Console.WriteLine($"Number of items: {count}");
+        Console.WriteLine($"Top Item: {aStack.Peek()}");
+        if (aStack.Contains(search) == condT)
+        {
+            Console.WriteLine($"Stack contains \"{search}\": True");
+            do
+            {
+                aStack.Pop();
+            } while (aStack.Contains(search) == condT && aStack.Count > 0);
+        }
+        else
+            Console.WriteLine($"Stack constains {search}: False");
+        aStack.Push(newItem);
+        return aStack;
+    }
+}
