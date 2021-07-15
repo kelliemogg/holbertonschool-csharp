120f0bd09a0db3bec258653d7c4f781a48270f39
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-15 17:09:05 +0000

    using IntersectWith to find common values

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        HashSet<int> set1 = new HashSet<int>(list1);
+        HashSet<int> set2 = new HashSet<int>(list2);
+        HashSet<int> newHash = new HashSet<int>(set1);
+        List<int> newList = new List<int>();
+
+        newHash.IntersectWith(set2);
+        foreach (int i in newHash)
+            newList.Add(i);
+        return newList;
+    }
+}
