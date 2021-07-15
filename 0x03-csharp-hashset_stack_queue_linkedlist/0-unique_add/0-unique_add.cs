c1ff446b49fd99ed1338172ccf3913f517f94f36
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-15 16:44:22 +0000

    Unique num sum

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        int total = 0;
+        HashSet<int> uniqueNumbers = new HashSet<int>(myList);
+        uniqueNumbers = myList.ToHashSet();
+        
+        foreach (int value in uniqueNumbers)
+        {
+            total += value;
+        }
+        return total;
+    }
+}
