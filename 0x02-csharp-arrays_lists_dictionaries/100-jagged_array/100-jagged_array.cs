f1ea3a9be7e96246c1c8a3e851d4b59c0cf13044
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-15 03:19:24 +0000

    nested loops for a jaggedArray

---
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _100_jagged_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] jaggedArray = new int[3][];
+            jaggedArray[0] = new int[] {0, 1, 2, 3};
+            jaggedArray[1] = new int[] {0, 1, 2, 3, 4, 5, 6};
+            jaggedArray[2] = new int[] {0, 1};
+
+            foreach (int[] row in jaggedArray)
+            {
+                foreach (int column in row)
+                {
+                    if (column == row.Length)
+                        Console.WriteLine(column);
+                    else
+                        Console.Write($"{column} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
