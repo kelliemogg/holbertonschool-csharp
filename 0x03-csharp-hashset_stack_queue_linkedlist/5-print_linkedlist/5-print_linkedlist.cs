9b5ede7681187ee1c69da703c61b8a20acdac0bd
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-15 22:28:26 +0000

    adding values to a linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> linx = new LinkedList<int>();
+    
+        if (size < 0)
+            return (linx);
+        for (int i = 0; i < size; i++)
+        {
+            linx.AddLast(i);
+            Console.WriteLine(i);
+        }
+        return (linx);
+    }
+}
