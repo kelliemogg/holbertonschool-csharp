520423b3a38d602f5e703a952e40f7bca79c9ba7
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-16 00:04:07 +0000

    adding values of a linked list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int i = 0;
+        foreach (int val in myLList)
+        {
+            i += val;
+        }
+        return i;
+    }
+}
