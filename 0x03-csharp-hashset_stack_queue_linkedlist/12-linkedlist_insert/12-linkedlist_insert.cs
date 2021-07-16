9aece941a60465994908be0416f2e3dacc9c292d
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-16 00:48:42 +0000

    End of LL needs no node creation

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> nood = myLList.First;
+
+        for (; nood != null; nood = nood.Next)
+        {
+            if (nood.Value > n)
+            {
+                myLList.AddBefore(nood, n);
+                return nood;
+            }
+        }
+        myLList.AddLast(n);
+        return nood;
+    }
+}
