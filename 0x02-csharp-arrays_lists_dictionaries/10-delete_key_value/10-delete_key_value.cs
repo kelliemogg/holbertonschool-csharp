e4a7931358fcdd52ce02d7a56cd49d898c19282b
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-16 18:44:15 +0000

    remove key

--- 0x02-csharp-arrays_lists_dictionaries/10-delete_key_value/10-delete_key_value.cs
+++ 0x02-csharp-arrays_lists_dictionaries/10-delete_key_value/10-delete_key_value.cs
@@ -1,20 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class Dictionary
 {
     public static Dictionary<string, string> DeleteKeyValue(Dictionary<string, string> myDict, string key)    
     {
-        if (myList.Count == 0)
-        {
-            Console.WriteLine("List is empty");
-            return -1;
-        }
-        int max_num = myList[0];
-        foreach (int i in myList)
-        {
-            if (max_num < i)
-                max_num = i;
-        }
-        return max_num;
+        myDict.Remove(key);
+        return myDict;
     }
 }
