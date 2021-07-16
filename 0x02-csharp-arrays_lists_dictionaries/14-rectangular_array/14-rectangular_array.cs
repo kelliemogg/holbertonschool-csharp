e1cfd85351ee4f7e6bcf9d44eea2b42e2071ed4a
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-16 15:21:36 +0000

    matrix

--- 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        int w = 5;
+        int h = 5;
+        int[,] twod = new int[w, h];
+        twod[2,2] = 1;
+
+        for (int x = 0; x < w; x++)
+        {
+            for (int y = 0; y < h; y++)
+            {
+                Console.Write (twod [x, y] + " ");
+            }
+            Console.WriteLine ();
+        }
     }
 }
