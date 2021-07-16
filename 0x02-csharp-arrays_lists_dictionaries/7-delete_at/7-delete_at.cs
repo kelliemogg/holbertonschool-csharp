60e25a25723dcd9a7a8154a68f75085b09e78c6d
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-16 17:00:03 +0000

    specified index

--- 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -6,7 +6,7 @@ class List
 {
     public static List<int> DeleteAt(List<int> myList, int index)
     {
-        if (index <= 0 || index > myList.Count)
+        if (index < 0 || index >= myList.Count)
         {
             Console.WriteLine("Index is out of range");
             return myList;
@@ -15,7 +15,7 @@ class List
         {
             if (i == index)
             {
-                myList.Remove(i + 1);
+                myList.Remove(myList[index]);
             }
         }
         return myList;
