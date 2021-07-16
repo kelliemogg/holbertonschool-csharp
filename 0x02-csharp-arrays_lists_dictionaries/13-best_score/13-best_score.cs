c17871f03a9b3f20fb7c90bf0d2777762ee5deed
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-16 20:50:25 +0000

    best score using sort by method

--- 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -1,20 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Dictionary
 {
     public static string BestScore(Dictionary<string, int> myList)
     {
-        if (myList.Count == 0)
+        if (myList.Count > 0)
         {
-            Console.WriteLine("List is empty");
-            return -1;
+            var result = myList.OrderByDescending(x => x.Value).First();
+            return result.Key;
         }
-        int max_num = myList[0];
-        foreach (int i in myList)
+        else
         {
-            if (max_num < i)
-                max_num = i;
+            return "None";
         }
-        return max_num;
     }
 }
