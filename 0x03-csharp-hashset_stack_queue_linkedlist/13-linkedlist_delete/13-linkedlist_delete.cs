7e1d93cfab9441868a4c33e939d17841d03d3bb5
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-16 01:07:51 +0000

    deleting nodes

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        LinkedListNode<int> nood = myLList.First;
+        for (int i = 0; i <= index && nood != null; i++)
+        {
+            if (i != index)
+            {
+                nood = nood.Next;
+                continue;
+            }
+            myLList.Remove(nood);
+        }
+    }
+}
