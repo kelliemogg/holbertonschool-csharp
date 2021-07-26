6ea319e50ef197e38efc9513dbf5e3624702b705
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-26 18:41:06 +0000

    try catch looping through two lists

---
+++ 0x04-csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
+    {
+        List<int> results = new List<int>();
+        int res = 0;
+        int one = 0;
+        for (one = 0; one < listLength; one++)
+        {
+            try
+            {
+                res = list1[one] / list2[one];
+                results.Add(res);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Out of range");
+                results.Add(0);
+            }
+        }
+        return (results);
+    }
+}
