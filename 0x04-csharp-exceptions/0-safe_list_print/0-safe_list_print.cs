8afc4d46587a7e9993a998669a11a08364f5277a
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-26 17:16:40 +0000

    base file

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        for (int i = 0; i < myList.Count; i++)
+        {
+            try
+            {
+                if (n < myList.Count)
+                {
+                    Console.WriteLine(i);
+                }
+                return (i);
+            }
+            catch (System.Exception)
+            {
+                foreach (int val in myList)
+                {
+                    Console.WriteLine(val);
+                }
+            }
+        }
+        return (n);
+    }
+}
