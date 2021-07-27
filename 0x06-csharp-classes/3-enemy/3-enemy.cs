1ef2bc15026df023967396422104b625a49f9e99
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-27 18:11:28 +0000

    Copied from 2

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Defines class Zombie 
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Creates public field health
+        /// </summary>
+        public int health;
+        /// <summary>
+        /// Initialize new instance
+        /// </summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+        /// <summary>
+        /// instance of Zombie with params passed
+        /// </summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+                this.health = value;
+        }
+    }
+}
