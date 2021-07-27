5bc58c41705116deae78c7e11a94591d13da821e
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-27 18:33:57 +0000

    Correct output and XML documentation

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Defines class Zombie 
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Creates private field health
+        /// </summary>
+        private int health;
+        /// <summary>
+        /// Creates private field name
+        /// </summary>
+        private string name;
+        /// <summary>
+        /// Creates public Zombie obj
+        /// </summary>
+        public Zombie()
+        {
+            this.health = 0;
+            this.name = "(No name)";
+        }
+        /// <summary>
+        /// instance of Zombie with params passed
+        /// </summary>
+        public Zombie(int value)
+        {
+            if (value >= 0)
+                this.health = value;
+        }
+        /// <summary>
+        /// Gets Health of Zombie object
+        /// </summary>
+
+        /// <summary>
+        /// gets and sets values for Name
+        /// </summary>
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        /// <summary>
+        /// returns the Health for a zombie
+        /// </summary>
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}
