cacfe64698a6e84404080f6676228ca9487ba591
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-27 17:49:27 +0000

    Namespace with a public class and class constructor

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Defines class Zombie 
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Creates public field health
+        /// </summary>
+        public int health;
+        /// <summary>
+        /// Initialize new instance
+        /// </summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+    }
+}
