1290f3178617bcb04b7e1a1b9cdeef0d14b91080
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-29 17:25:35 +0000

    updated unit test

--- 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -6,18 +6,12 @@ namespace MyMath.Tests
     /// <summary> Tests for MyMath divide </summary>
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
-        public void Test1()
+        public void TestNull()
         {
-            int a = 10;
-            int b = 5;
-            int res = Operations.Divide(a, b);
-            Assert.AreEqual(2, res);
+            int[,] mat = null;
+            int[,] res = Matrix.Divide(mat, 10);
+            Assert.AreEqual(null, res);
         }
     }
 }
