2b360269fa61bebd8cb19d850dd4e915ae37b18d
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-29 17:13:56 +0000

    divide elememts in a matrix

---
+++ 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MyMath
+{
+    public class Matrix
+    {
+        public static int[,] Divide(int[,] matrix, int num)
+        {
+            int a, b = 0;
+            int array1 = matrix.GetLength(0);
+            int array2 = matrix.GetLength(1);
+            int [,] newMatrix = new int[array1, array2];
+            if (matrix == null)
+            {
+                return null;
+            }
+            try
+            {
+                for (a = 0; a < array1; a++)
+                {
+                    for (b = 0; b < array2; b++)
+                    {
+                        newMatrix = matrix[a, b] /= num;
+                    }
+                }
+            }
+            catch (System.Exception)
+            {
+                Console.WriteLine("Num cannot be 0");
+                return null;
+            }
+            return newMatrix;
+        }
+    }
+}
