a51bcf24e37bdac10ae1c784267e21dba2f271a8
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-29 23:22:42 +0000

    Added unit tests

--- 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -3,18 +3,66 @@ using System.Collections.Generic;
 
 namespace MyMath.Tests
 {
-    [TestClass]
+    [TestFixture]
     /// <summary> Tests for max_int </summary>
     public class OperationsTests
     {
-        [TestMethod]
-        public void Max()
+        [Test]
+        public void Max_end()
         {
             List<int> testList = new List<int>();
-            testList.AddRange(Enumerable.Range(1, 10));
+            testList.Add(5);
+            testList.Add(7);
+            testList.Add(9);
+    
             int results = Operations.Max(testList);
 
-            Assert.AreEqual(10, output);
+            Assert.AreEqual(9, results);
+        }
+        [Test]
+        public void Max_beginning()
+        {
+            List<int> testList = new List<int>();
+            testList.Add(9);
+            testList.Add(7);
+            testList.Add(5);
+
+            int results = Operations.Max(testList);
+
+            Assert.AreEqual(9, results);
+        }
+        [Test]
+        public void Max_middle()
+        {
+            List<int> testList = new List<int>();
+            testList.Add(5);
+            testList.Add(9);
+            testList.Add(7);
+
+            int results = Operations.Max(testList);
+
+            Assert.AreEqual(9, results);
+        }
+        [Test]
+        public void Max_negative()
+        {
+            List<int> newList = new List<int>();
+            testList.Add(-5);
+            testList.Add(-7);
+            testList.Add(-9);
+
+            int results = Operations.Max(testList);
+
+            Assert.AreEqual(-5, results);
+        }
+        [Test]
+        public void Max_empty()
+        {
+            List<int> newList = new List<int>();
+            
+            int results = Operations.Max(testList);
+
+            Assert.AreEqual(0, results);
         }
     }
 }
