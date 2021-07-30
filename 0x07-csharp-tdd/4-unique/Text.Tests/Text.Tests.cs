b2c4341540c8d99b07063f06f733785a6f3b38fc
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-30 17:15:30 +0000

    tests

--- 0x07-csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
@@ -13,5 +13,33 @@ namespace Text.Tests
             int result = Str.UniqueChar(test);
             Assert.AreEqual(3, result);
         }
+        [Test]
+        public void testfirst()
+        {
+            string test = "zaap";
+            int result = Str.UniqueChar(test);
+            Assert.AreEqual(0, result);
+        }
+        [Test]
+        public void testlast()
+        {
+            string test = "zzaap";
+            int result = Str.UniqueChar(test);
+            Assert.AreEqual(4, result);
+        }
+        [Test]
+        public void testnounique()
+        {
+            string test = "zzaapp";
+            int result = Str.UniqueChar(test);
+            Assert.AreEqual(-1, result);
+        }
+        [Test]
+        public void testempty()
+        {
+            string test = "";
+            int result = Str.UniqueChar(test);
+            Assert.AreEqual(-1, result);
+        }
     }
 }
