e3245945908f69bf802070616a42b2a428e03785
Kellie Mogg <kelliedmogg@gmail.com>
2021-07-30 16:53:38 +0000

    correct output

--- 0x07-csharp-tdd/4-unique/Text/Text.cs
+++ 0x07-csharp-tdd/4-unique/Text/Text.cs
@@ -10,13 +10,19 @@ namespace Text
         /// <summary> returns indx of second unique char </summary>
         public static int UniqueChar(string s)
         {
-            char first = s[0];
+            Dictionary<char,int> unFind = new Dictionary<char,int>();
+
             for (int i = 0; i < s.Length; i++)
             {
-                if (first != s[i] && s[i] != s[i + 1])
-                {
-                    return (i);
-                }
+                if (!unFind.ContainsKey(s[i]))
+                    unFind.Add(s[i], 1);
+                else
+                    unFind[s[i]] += 1;
+            }
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (unFind[s[i]] == 1)
+                    return i;
             }
             return -1;
         }
