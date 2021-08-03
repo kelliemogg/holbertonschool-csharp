c87a9a3d0cd7bb1c0e84ead1c3cc908b2df7f074
Kellie Mogg <kelliedmogg@gmail.com>
2021-08-03 20:12:13 +0000

    correct output

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+/// <summary> class obj test type and inheritance </summary>
+class Obj
+{
+    public static bool IsInstanceOfArray(object obj)
+    {
+        if (obj is Array)
+            return true;
+        else
+            return false;
+    }
+}
