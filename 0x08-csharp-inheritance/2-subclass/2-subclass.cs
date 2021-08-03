ac8b72b95095585415f7124b58433f0e58546f9d
Kellie Mogg <kelliedmogg@gmail.com>
2021-08-03 20:21:51 +0000

    typo - correct output identifying subclass

--- 0x08-csharp-inheritance/2-subclass/2-subclass.cs
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -5,7 +5,7 @@ class Obj
 {
     public static bool IsOnlyASubclass(Type derivedType, Type baseType)
     {
-        if (derivedType.IsSubClassOf(baseType))
+        if (derivedType.IsSubclassOf(baseType))
             return true;
         else
             return false;
