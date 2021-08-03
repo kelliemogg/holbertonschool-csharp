ad72295bcd1ae857edd6ea7296bed4ffe43fc2ee
Kellie Mogg <kelliedmogg@gmail.com>
2021-08-03 20:19:44 +0000

    not working

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+/// <summary> class obj </summary>
+class Obj
+{
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        if (derivedType.IsSubClassOf(baseType))
+            return true;
+        else
+            return false;
+    }
+}
