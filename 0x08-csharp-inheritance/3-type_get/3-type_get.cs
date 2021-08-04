01fb3fc84475c668852f8c1b7a076ec77afd486c
Kellie Mogg <kelliedmogg@gmail.com>
2021-08-04 01:44:12 +0000

    types of methods and properties

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+
+/// <summary> class Obj </summary>
+class Obj
+{
+    public static void Print(object myObj)
+    {
+        TypeInfo detailList = myObj.GetType().GetTypeInfo();
+
+        Console.WriteLine("{0} Properties:", detailList.Name);
+
+        foreach (PropertyInfo p in detailList.GetProperties())
+        {
+            Console.WriteLine(p.Name);
+        }
+        Console.WriteLine("{0} Methods:", detailList.Name);
+
+        foreach (MethodInfo m in detailList.GetMethods())
+        {
+            Console.WriteLine(m.Name);
+        }
+    }
+}
