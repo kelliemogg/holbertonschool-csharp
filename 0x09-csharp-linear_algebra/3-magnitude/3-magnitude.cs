7a5e125a884b8eac5dcce747fd57882d5904c111
kelliemogg <kelliedmogg@gmail.com>
2021-08-23 15:36:25 -0500

    vector math

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+
 /// <summary> Vector Math </summary> 
 
 class VectorMath
@@ -6,6 +7,27 @@ class VectorMath
     public static double Magnitude(double[] vector)
     {
         Double lengthResult = vector.Length;
-        Console.WriteLine(lengthResult);
+        if (lengthResult == 2)
+        {
+            double res2D = 0;
+            double result2D = 0;
+
+            result2D = ((vector[0] * vector[0]) + (vector[1] * vector[1]));
+            res2D = Math.Sqrt(result2D);
+            res2D = Math.Round(res2D, 2);
+            return res2D;
+        }
+        else if (lengthResult == 3)
+        {
+            double res3D = 0;
+            double result3D = 0;
+
+            result3D = ((vector[0] * vector[0]) + (vector[1] * vector[1]) + (vector[2] * vector[2]));
+            res3D = Math.Sqrt(result3D);
+            res3D = Math.Round(res3D, 2);
+            return res3D;
+        }
+        else
+            return -1;
     }
 }
