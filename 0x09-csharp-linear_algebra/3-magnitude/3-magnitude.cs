f606ee744867c5f1cc298dcb7ae86a96b4c49aad
kelliemogg <kelliedmogg@gmail.com>
2021-08-23 15:09:03 -0500

    getting access to main file

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,11 @@
+﻿using System;
+/// <summary> Vector Math </summary> 
+
+class VectorMath
+{
+    public static double Magnitude(double[] vector)
+    {
+        Double lengthResult = vector.Length;
+        Console.WriteLine(lengthResult);
+    }
+}
