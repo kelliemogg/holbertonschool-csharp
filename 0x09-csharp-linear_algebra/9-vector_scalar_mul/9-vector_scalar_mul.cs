4e1b396bdb2a4d485c4c85583a7b02dd21f2da43
kelliemogg <kelliedmogg@gmail.com>
2021-08-23 16:27:28 -0500

    correct output for mul vector by a scalar

--- 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -5,21 +5,21 @@ class VectorMath
     public static double[] Multiply(double[] vector, double scalar)
     {
         double[] not = {-1};
-        double lengthResult1 = vector1.Length;
+        double lengthResult1 = vector.Length;
 
         if (lengthResult1 == 2)
         {
             double[] result2D = {0, 0};
-            result2D[0] = vector1[0] * scalar;
-            result2D[1] = vector1[1] * scalar;
+            result2D[0] = vector[0] * scalar;
+            result2D[1] = vector[1] * scalar;
             return result2D;
         }
         else if (lengthResult1 == 3)
         {
             double[] result3D = {0, 0, 0};
-            result3D[0] = vector1[0] * scalar;
-            result3D[1] = vector1[1] * scalar;
-            result3D[2] = vector1[2] * scalar;
+            result3D[0] = vector[0] * scalar;
+            result3D[1] = vector[1] * scalar;
+            result3D[2] = vector[2] * scalar;
             return result3D;
         }
         else
