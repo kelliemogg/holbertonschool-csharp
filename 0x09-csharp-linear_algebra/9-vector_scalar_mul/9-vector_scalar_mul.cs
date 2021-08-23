9fbf750ba004b9f41741972691016f48f5d97e66
kelliemogg <kelliedmogg@gmail.com>
2021-08-23 16:24:29 -0500

    need main file

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class VectorMath
+{
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        double[] not = {-1};
+        double lengthResult1 = vector1.Length;
+
+        if (lengthResult1 == 2)
+        {
+            double[] result2D = {0, 0};
+            result2D[0] = vector1[0] * scalar;
+            result2D[1] = vector1[1] * scalar;
+            return result2D;
+        }
+        else if (lengthResult1 == 3)
+        {
+            double[] result3D = {0, 0, 0};
+            result3D[0] = vector1[0] * scalar;
+            result3D[1] = vector1[1] * scalar;
+            result3D[2] = vector1[2] * scalar;
+            return result3D;
+        }
+        else
+            return not;
+    }
+}
+
