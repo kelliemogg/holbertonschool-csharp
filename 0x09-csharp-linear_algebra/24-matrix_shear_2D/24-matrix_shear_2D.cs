56bdd06b2dbfb22d4e9b3947551f3eb5e21218c1
kelliemogg <kelliedmogg@gmail.com>
2021-08-25 17:01:31 -0500

    base files

---
+++ 0x09-csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Shear2D(double[,] matrix, char direction, double factor)
+    {
+        double[,] not = {{ -1 }};
+            return not;
+    }
+}
