f72035b805dca5850e6893895260f7a880174720
kelliemogg <kelliedmogg@gmail.com>
2021-08-25 16:53:44 -0500

    base files

---
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Rotate2D(double[,] matrix, double angle)
+    {
+        double[,] not = {{ -1 }};
+            return not;
+    }
+}
