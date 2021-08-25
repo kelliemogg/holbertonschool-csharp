eedc1c56f6fe6a90f21e3f135e2a8fe64ebecd37
kelliemogg <kelliedmogg@gmail.com>
2021-08-25 16:51:49 -0500

    base files

---
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+    {
+        double[,] not = {{ -1 }};
+            return not;
+    }
+}
