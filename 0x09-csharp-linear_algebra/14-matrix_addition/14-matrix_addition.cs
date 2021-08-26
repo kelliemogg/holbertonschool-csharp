9b898d87670438b35d32a32f9b160411eca6266f
kelliemogg <kelliedmogg@gmail.com>
2021-08-26 14:54:43 -0500

    task 14

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -10,33 +10,83 @@ class MatrixMath
     public static double[,] Add(double[,] matrix1, double[,] matrix2)
     {
         double[,] not = {{ -1 }};
-        double lengthResult1 = matrix1.GetLength(0);
-        double lengthResult2 = matrix2.GetLength(0);
+        double[,] newMatrix;
+        double len1 = matrix1.GetLength(0);
+        double len2 = matrix1.GetLength(1);
+        double seclen1 = matrix2.GetLength(0);
+        double seclen2 = matrix2.GetLength(1);
+        int i, j = 0;
 
-        if (lengthResult1 == 2 && lengthResult2 == 2)
+        /* first determine size of resulting matrix */
+        if (len1 == 2 && len2 == 2 && seclen1 == 2 && seclen2 == 2)
         {
-            double[,] result2D = {{ 0 }};
-            result2D[0, 0] = matrix1[0, 0] + matrix2[0, 0];
-            result2D[0, 1] = matrix1[0, 1] + matrix2[0, 1];
-            result2D[1, 0] = matrix1[1, 0] + matrix2[1, 0];
-            result2D[1, 1] = matrix1[1, 1] + matrix2[1, 1];
-            return result2D;
+            newMatrix = new double[2, 2];
+            for (i = 0; i < len1; i++)
+            {
+                for (j = 0; j < len2; j++)
+                {
+                    newMatrix[i, j] = matrix1[i, j] + matrix2[i, j];
+                }
+            }
+            return newMatrix;
         }
-        else if (lengthResult1 == 3 && lengthResult2 == 3)
+        else if (len1 == 3 && len2 == 3 && seclen1 == 3 && seclen2 == 3)
         {
-            double[,] result3D = {{ 0 }};
-            result3D[0, 0] = matrix1[0, 0] + matrix2[0, 0];
-            result3D[0, 1] = matrix1[0, 1] + matrix2[0, 1];
-            result3D[0, 2] = matrix1[0, 2] + matrix2[0, 2];
-            result3D[1, 0] = matrix1[1, 0] + matrix2[1, 0];
-            result3D[1, 1] = matrix1[1, 1] + matrix2[1, 1];
-            result3D[1, 2] = matrix1[1, 2] + matrix2[1, 2];
-            result3D[2, 0] = matrix1[2, 0] + matrix2[2, 0];
-            result3D[2, 1] = matrix1[2, 1] + matrix2[2, 1];
-            result3D[2, 2] = matrix1[2, 2] + matrix2[2, 2];
-            return result3D;
+            newMatrix = new double[2, 2];
+            for (i = 0; i < len1; i++)
+            {
+                for (j = 0; j < len2; j++)
+                {
+                    newMatrix[i, j] = matrix1[i, j] + matrix2[i, j];
+                }
+            }
+            return newMatrix;
         }
         else
             return not;
     }
 }
+
+
+// using System;
+// using System.Collections.Generic;
+
+// /// <summary>
+// /// Vector Math
+// /// </summary>
+
+// class MatrixMath
+// {
+//     public static double[,] Add(double[,] matrix1, double[,] matrix2)
+//     {
+//         double[,] not = {{ -1 }};
+//         double lengthResult1 = matrix1.GetLength(0);
+//         double lengthResult2 = matrix2.GetLength(0);
+
+//         if (lengthResult1 == 2 && lengthResult2 == 2)
+//         {
+//             double[,] result2D = {{ 0 }};
+//             result2D[0, 0] = matrix1[0, 0] + matrix2[0, 0];
+//             result2D[0, 1] = matrix1[0, 1] + matrix2[0, 1];
+//             result2D[1, 0] = matrix1[1, 0] + matrix2[1, 0];
+//             result2D[1, 1] = matrix1[1, 1] + matrix2[1, 1];
+//             return result2D;
+//         }
+//         else if (lengthResult1 == 3 && lengthResult2 == 3)
+//         {
+//             double[,] result3D = {{ 0 }};
+//             result3D[0, 0] = matrix1[0, 0] + matrix2[0, 0];
+//             result3D[0, 1] = matrix1[0, 1] + matrix2[0, 1];
+//             result3D[0, 2] = matrix1[0, 2] + matrix2[0, 2];
+//             result3D[1, 0] = matrix1[1, 0] + matrix2[1, 0];
+//             result3D[1, 1] = matrix1[1, 1] + matrix2[1, 1];
+//             result3D[1, 2] = matrix1[1, 2] + matrix2[1, 2];
+//             result3D[2, 0] = matrix1[2, 0] + matrix2[2, 0];
+//             result3D[2, 1] = matrix1[2, 1] + matrix2[2, 1];
+//             result3D[2, 2] = matrix1[2, 2] + matrix2[2, 2];
+//             return result3D;
+//         }
+//         else
+//             return not;
+//     }
+// }
