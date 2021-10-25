1d1b12c18c747f93bedf0cb906643288db5171c9
kelliemogg <kelliedmogg@gmail.com>
2021-10-25 09:37:49 -0800

    XML doc

--- 0x0A-csharp-generics/0-queue/queue.cs
+++ 0x0A-csharp-generics/0-queue/queue.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+
 /// <summary>
 ///Generic data type of class Queue
 /// </summary>
+
 public class Queue<T>
 {
-    /// <summary> defines generic class and checks the type </summary> ///
+    /// <summary>
+    /// defines generic class and checks the type 
+    /// </summary>
     public Type CheckType() 
     {
         return typeof(T);
