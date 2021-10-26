6021e66064924f075230f16f49c825828b81f6ba
kelliemogg <kelliedmogg@gmail.com>
2021-10-26 15:16:27 -0800

    Overriding ToString method in abstract class

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/// <summary>
+/// abstract class Base
+/// </summary>
+public abstract class Base
+{
+    /// <summary>
+    /// declare public string name
+    /// </summary>
+    public string name;
+    /// <summary>
+    /// overriding ToString
+    /// </summary>
+    public override string ToString()
+    {
+        return name + " is a " + GetType().Name;
+    }
+}
