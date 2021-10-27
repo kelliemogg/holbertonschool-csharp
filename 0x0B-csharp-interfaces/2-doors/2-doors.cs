5168fa14b95dbb8cfa22cc5cc8b147cc2dec709f
kelliemogg <kelliedmogg@gmail.com>
2021-10-26 16:41:04 -0800

    Adding a new class Door that inherits from Base and Interface Interactive

---
+++ 0x0B-csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+/// <summary>
+/// abstract class Base
+/// </summary>
+public abstract class Base
+{
+    /// <summary>
+    /// declare public string name
+    /// </summary>
+    public string name { get; set; }
+    /// <summary>
+    /// overriding ToString
+    /// </summary>
+    public override string ToString()
+    {
+        return name + " is a " + GetType().Name;
+    }
+}
+
+/// <summary>
+/// Interface Interactive
+/// </summary>
+public interface IInteractive
+{
+    /// <summary>
+    /// declare public void Interact()
+    /// </summary>
+    void Interact();
+}
+/// <summary>
+/// Interface Breakable
+/// </summary>
+public interface IBreakable
+{
+    /// <summary>
+    /// declare property durability
+    /// </summary>
+    int durability { get; set; }
+    /// <summary>
+    /// declare public void Break()
+    /// </summary>
+    void Break();
+}
+/// <summary>
+/// Interface Collectable
+/// </summary>
+public interface ICollectable
+{
+    /// <summary>
+    /// property isCollected
+    /// </summary>
+    bool isCollected { get; set; }
+    /// <summary>
+    /// declare public void Collect()
+    /// </summary>
+    void Collect();
+}
+/// <summary>
+/// class Door
+/// </summary>
+public class Door : Base, IInteractive
+{
+    /// <summary>
+    /// constructor
+    /// </summary>
+    public Door(string name = "Door")
+    {
+        this.name = name;
+    }
+    /// <summary>
+    /// override Interact
+    /// </summary>
+    public void Interact()
+    {
+        Console.WriteLine("You try to open the " + this.name + ". It's locked.");
+    }
+}
